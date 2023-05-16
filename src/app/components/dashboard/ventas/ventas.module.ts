3ebedaea23e814d7eb125970c8678ba76db42b62
¨Abel¨ <randres135@gmail.com>
2023-05-16 17:40:06 -0600

    se arreglaron conflitos de pull en modulo venta

--- src/app/components/dashboard/ventas/ventas.module.ts
+++ src/app/components/dashboard/ventas/ventas.module.ts
@@ -11,13 +11,9 @@ import { VentaListComponent } from './venta/venta-list/venta-list.component';
 import { CotizacionComponent } from './venta/cotizacion/cotizacion.component';
 import { ProductoComponent } from './producto/producto.component';
 import { VistaComponent } from './venta/vista/vista.component';
-<<<<<<< HEAD
 import { CuentasPorCobrarComponent } from './creditos/cuentas-por-cobrar/cuentas-por-cobrar.component';
 import { PrimeModule } from '../../prime/prime.module';
 import { FormsModule } from '@angular/forms';
-=======
-import { PrimeModule } from '../../prime/prime.module';
->>>>>>> a1da166b3c8bcbb92928b11ea83b5f3999100fee
 
 
 
@@ -37,14 +33,11 @@ import { PrimeModule } from '../../prime/prime.module';
   imports: [
     CommonModule,
     SharedModule,
-<<<<<<< HEAD
     VentasRoutingModule, 
     PrimeModule, 
-    FormsModule
-=======
+    FormsModule,
     VentasRoutingModule,
     PrimeModule,
->>>>>>> a1da166b3c8bcbb92928b11ea83b5f3999100fee
   ]
 })
 export class VentasModule { }
